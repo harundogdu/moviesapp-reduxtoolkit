66d1a63f7388ed26e4a14fe6104362e1f075554b
Harun Doğdu <harundogdu06@gmail.com>
2022-01-19 01:04:48 +0300

    no found page designed

--- src/components/NoFoundPage/NoFoundPage.jsx
+++ src/components/NoFoundPage/NoFoundPage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import "./NoFoundPage.scss";
+import { AiOutlineLeft } from "react-icons/ai";
+import { Link } from "react-router-dom";
 const NoFoundPage = () => {
-    return (
-        <div>
-            NoFoundPage
-        </div>
-    )
-}
+  return (
+    <div className="nofoundpage">
+      <h1>404 Page Not Found</h1>
+      <h3>Sorry, the page you were looking for doesn't exist.</h3>
+      <Link to="/">
+        <AiOutlineLeft size={24} />
+        Go back
+      </Link>
+    </div>
+  );
+};
 
-export default NoFoundPage
+export default NoFoundPage;
