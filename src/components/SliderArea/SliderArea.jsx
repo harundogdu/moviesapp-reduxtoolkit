567136d35e8583572d9fbc958a1a400894f0007a
Harun Doğdu <harundogdu06@gmail.com>
2022-01-19 01:06:18 +0300

    slider area component changed

--- src/components/SliderArea/SliderArea.jsx
+++ src/components/SliderArea/SliderArea.jsx
@@ -6,8 +6,18 @@ import "slick-carousel/slick/slick-theme.css";
 import { Link } from "react-router-dom";
 import "./SliderArea.scss";
 import { Loading } from "components/export";
+import DefaultImage from "images/default-image.jpg";
 /* function */
-const SliderArea = ({ title, data, loading, type }) => {
+const SliderArea = ({ title, data, loading }) => {
+  const imageLast = (item) => {
+    let baseURL = "https://image.tmdb.org/t/p/w500/";
+    if (item.poster_path) {
+      return (baseURL += item.poster_path);
+    } else if (item.profile_path) {
+      return (baseURL += item.profile_path);
+    }
+    return DefaultImage;
+  };
   return (
     <>
       {loading ? (
@@ -18,14 +28,12 @@ const SliderArea = ({ title, data, loading, type }) => {
           <Slider className="slider" {...Settings}>
             {data.map((item) => (
               <Link
-                to={`/details/${type}-${item.id}`}
+                to={`/details/${item.media_type}-${item.id}`}
                 className="home-container-item"
                 key={item.id}
               >
                 <img
-                  src={`https://image.tmdb.org/t/p/w500${
-                    item.poster_path ? item.poster_path : item.profile_path
-                  }`}
+                  src={imageLast(item)}
                   alt={item.title ? item.title : item.name}
                 />
                 <span>{item.title ? item.title : item.name}</span>
